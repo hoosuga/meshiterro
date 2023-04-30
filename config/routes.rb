095e7fc57ebe2f8375b13f7d8fddc3af6bc08953
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-04-30 07:42:50 +0000

    [Add] 4-18 コメント投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
+  get 'homes/about',as:'about'
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create]
+  end
+
   resources :users, only: [:show, :edit, :update]
 
-  get 'homes/about',as:'about'
 end
